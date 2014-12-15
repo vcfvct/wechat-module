4a533f2f33abb54fb9b0a783b81f7b15eadba623
leon <vcfvct@gmail.com>
2014-12-14 19:09:03 -0500

    try to add log4j in openshift

--- src/main/java/com/goodtrendltd/wechat/WebInitializer.java
+++ src/main/java/com/goodtrendltd/wechat/WebInitializer.java
@@ -5,24 +5,36 @@ import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.util.Log4jConfigListener;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 
-public class WebInitializer implements WebApplicationInitializer {
+public class WebInitializer implements WebApplicationInitializer
+{
 
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
+    public void onStartup(ServletContext servletContext) throws ServletException
+    {
+        //spring dispatcher
         WebApplicationContext context = getContext();
         servletContext.addListener(new ContextLoaderListener(context));
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
+
+        //log4j config
+        servletContext.setInitParameter("log4jConfigLocation", "classpath:config/log4j.xml");
+        servletContext.setInitParameter("log4jRefreshInterval", "10000");
+        servletContext.setInitParameter("log4jExposeWebAppRoot", "false");
+        Log4jConfigListener log4jListener = new Log4jConfigListener();
+        servletContext.addListener(log4jListener);
     }
 
-    private AnnotationConfigWebApplicationContext getContext() {
+    private AnnotationConfigWebApplicationContext getContext()
+    {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.setConfigLocation("com.goodtrendltd.wechat");
         return context;
--- src/main/java/com/goodtrendltd/wechat/controller/UiController.java
+++ src/main/java/com/goodtrendltd/wechat/controller/UiController.java
@@ -1,8 +1,11 @@
 package com.goodtrendltd.wechat.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
 
 /**
  * Created by LeOn on 12/14/14.
@@ -10,9 +13,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UiController
 {
-    @RequestMapping("/index")
+    private final static Logger logger = Logger.getLogger(UiController.class);
+
+    @RequestMapping(value = {"/", "/index"})
     public ModelAndView handleIndex()
     {
+        logger.info("someone vistied the app at: " + new Date());
         ModelAndView mv = new ModelAndView("index");
         return mv;
     }
